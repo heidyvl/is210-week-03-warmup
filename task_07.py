556552cf4d0a1bbb7845793290dd65ca7a6eeb9d
heiydvl <heidy.villarreal@spsmail.cuny.edu>
2015-02-11 04:00:26 +0000

    granaries

--- task_07.py
+++ task_07.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """Asks the great question."""
-
+GRANARIES_EXIST=None;
 import task_06
 
 WORDS = task_06.WORDS
+if "granaries" in WORDS: GRANARIES_EXIST='TRUE';
+
+print GRANARIES_EXIST
