9c27ccca067fafaa1507210d8b0b409fcc2f664e
heiydvl <heidy.villarreal@spsmail.cuny.edu>
2015-02-12 21:13:32 +0000

    Mod

--- task_09.py
+++ task_09.py
@@ -5,9 +5,7 @@
 THE_ANSWER_TO_EVERYTHING = 42
 
 INFINITE_IMPROBABILITY = 'browning motion'
-"""This is a two
-
-line docstring"""
+"""This is a two \n line docstring"""
 
 import task_09
 help(task_09)
