df0c07b24484c33222ac7b5e5c424e482967bf3c
heiydvl <heidy.villarreal@spsmail.cuny.edu>
2015-02-12 19:48:10 +0000

    Dec and frac

---
+++ task_12.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Task 12"""
+from fractions import Fraction
+from decimal import *
+
+INTVAL = 1
+FLOATVAL = Decimal('0.1')
+DECVAL = Decimal(1)/Decimal(10)
+FRACVAL = Fraction('1/10')
+
+print INTVAL
+print FLOATVAL
+print DECVAL
+print FRACVAL
+
