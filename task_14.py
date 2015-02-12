f7b8f8a9f12e5170eae6900d7f80de79dd012a32
heiydvl <heidy.villarreal@spsmail.cuny.edu>
2015-02-12 20:11:11 +0000

    Mod

---
+++ task_14.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+IS_TRUE = True;
+IS_FALSE = False;
+IS_NONE = None;
+INTEGER_EQUIV = IS_TRUE == 1 & IS_FALSE == 0;
+
+print 'IS_TRUE', IS_TRUE;
+print 'IS_FALSE', IS_FALSE ;
+print 'IS_NONE', IS_NONE;
+print 'INTEGER_EQUIV', INTEGER_EQUIV; 
