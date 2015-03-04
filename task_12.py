f51c12c7e458a8b887476afdf37dec1a09dba8b8
heiydvl <heidy.villarreal@spsmail.cuny.edu>
2015-03-04 02:00:59 +0000

    mod

--- task_12.py
+++ task_12.py
@@ -3,15 +3,13 @@
 
 """Task 12"""
 from fractions import Fraction
-from decimal import *
-getcontext().prec = 1
+from decimal import Decimal
 INTVAL = 1
-FLOATVAL = round(Decimal(0.1),1)
+FLOATVAL = round(Decimal(0.1), 1)
 DECVAL = Decimal(1)/Decimal(10)
 FRACVAL = Fraction('1/10')
 
 print INTVAL
 print FLOATVAL
 print DECVAL
-print FRACVAL;
-
+print FRACVAL
