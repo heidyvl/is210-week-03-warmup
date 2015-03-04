4846b225bd9f8bf09e0ff6c8915bd2e0a270b9b8
heiydvl <heidy.villarreal@spsmail.cuny.edu>
2015-03-04 02:02:40 +0000

    Mod

--- task_14.py
+++ task_14.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""This is a docstring"""
 
-IS_TRUE = True;
-IS_FALSE = False;
-IS_NONE = None;
+IS_TRUE = True
+IS_FALSE = False
+IS_NONE = None
 INTEGER_EQUIV = IS_TRUE == 1 and IS_FALSE == 0
-
 
 print 'IS_TRUE', IS_TRUE
 print 'IS_FALSE', IS_FALSE
 print 'IS_NONE', IS_NONE
-print 'INTEGER_EQUIV', INTEGER_EQUIV 
+print 'INTEGER_EQUIV', INTEGER_EQUIV
