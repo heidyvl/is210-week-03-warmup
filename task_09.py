3dfbeb42fd0158a52f97927cd16338fbdb7f1e20
heiydvl <heidy.villarreal@spsmail.cuny.edu>
2015-03-04 02:00:22 +0000

    mod

--- task_09.py
+++ task_09.py
@@ -1,14 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """A computer that can build the question.
-This is a 
+This is a
 Docstring """
 
 THE_ANSWER_TO_EVERYTHING = 42
-
 INFINITE_IMPROBABILITY = 'browning motion'
-
-
-
-import task_09
-help(task_09)
